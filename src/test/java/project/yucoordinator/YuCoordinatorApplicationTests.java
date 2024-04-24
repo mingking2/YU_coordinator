10e79385a5e370998052fec91b1534f9dbddf72f
주민기 <mingking@jumingiui-noteubug.local>
2024-04-24 20:25:39 +0900

    init

---
+++ src/test/java/project/yucoordinator/YuCoordinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package project.yucoordinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuCoordinatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
