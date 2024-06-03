e491813970390bf273af10b09e97f4aa5a2445df
mingking2 <mingee641@gmail.com>
2024-06-04 08:58:48 +0900

    home 컨트롤러 클래스 추가

---
+++ src/main/java/com/project/yucoordinator/domain/common/HomeController.java
@@ -0,0 +1,25 @@
+package com.project.yucoordinator.domain.common;
+
+import com.project.yucoordinator.domain.info.entity.CSEInfoEntity;
+import com.project.yucoordinator.domain.info.entity.YUInfoEntity;
+import com.project.yucoordinator.domain.info.service.InfoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+    private final InfoService infoService;
+    @GetMapping("/")
+    public String home(Model model) {
+        List<YUInfoEntity> allYUInfoEntity = (List<YUInfoEntity>) infoService.findbyAllInfos(0);
+        List<CSEInfoEntity> allCSEInfoEntity = (List<CSEInfoEntity>) infoService.findbyAllInfos(1);
+        model.addAttribute("infoList", allYUInfoEntity);
+        model.addAttribute("CSEinfoList", allCSEInfoEntity);
+        return "home";
+    }
+}
