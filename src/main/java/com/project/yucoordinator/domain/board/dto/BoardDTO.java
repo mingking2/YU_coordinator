a3791f39f4edb4b8908b9dfed2d88ab0fd748e8b
mingking2 <mingee641@gmail.com>
2024-06-04 15:16:59 +0900

    BoardDTO 클래스 추가

---
+++ src/main/java/com/project/yucoordinator/domain/board/dto/BoardDTO.java
@@ -0,0 +1,39 @@
+package com.project.yucoordinator.domain.board.dto;
+
+import com.project.yucoordinator.domain.board.entity.BoardEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BoardDTO {
+    private Long id;
+    private String title;
+    private String url;
+    private String content;
+    private LocalDateTime createdTime;
+    private LocalDateTime updatedTime;
+
+
+    public static BoardDTO makeBoardDTO(BoardEntity boardEntity) {
+        return BoardDTO.builder()
+                .id(boardEntity.getId())
+                .title(boardEntity.getTitle())
+                .url(boardEntity.getUrl())
+                .content(boardEntity.getContent())
+                .createdTime(boardEntity.getCreatedTime())
+                .updatedTime(boardEntity.getUpdatedTime())
+                .build();
+    }
+
+    public void setDisplayId(int i) {
+        id = (long) i;
+    }
+
+}
