da3a95c19fa3989895a6fa75760f7f1d6fd08459
mingking2 <mingee641@gmail.com>
2024-06-04 15:20:04 +0900

    InfoDTO 클래스 추가

---
+++ src/main/java/com/project/yucoordinator/domain/info/dto/InfoDTO.java
@@ -0,0 +1,35 @@
+package com.project.yucoordinator.domain.info.dto;
+
+import com.project.yucoordinator.domain.info.entity.CSEInfoEntity;
+import com.project.yucoordinator.domain.info.entity.YUInfoEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class InfoDTO {
+    private Long id;
+    private String title;
+    private String url;
+
+
+    public static InfoDTO YuInfoDTO(YUInfoEntity yuInfoEntity) {
+        return InfoDTO.builder()
+                .id(yuInfoEntity.getId())
+                .title(yuInfoEntity.getTitle())
+                .url(yuInfoEntity.getUrl())
+                .build();
+    }
+
+    public static InfoDTO CseInfoDTO(CSEInfoEntity cseInfoEntity) {
+        return InfoDTO.builder()
+                .id(cseInfoEntity.getId())
+                .title(cseInfoEntity.getTitle())
+                .url(cseInfoEntity.getUrl())
+                .build();
+    }
+}
