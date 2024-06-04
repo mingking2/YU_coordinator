d1d865fcaf65bb190662c8de3a3e84727b7435c4
mingking2 <mingee641@gmail.com>
2024-06-04 09:48:20 +0900

    패키지 클래스명 변경

--- src/test/java/com/project/yucoordinator/YuCoordinatorApplicationTests.java
+++ src/test/java/com/project/yucoordinator/YuCoordinatorApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.project.yucoordinator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class YuCoordinatorApplicationTests {
 
     @Test
     void contextLoads() {
