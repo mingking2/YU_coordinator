739dc51ec8a44eef0587cb25e168b44e7269a33e
mingking2 <mingee641@gmail.com>
2024-06-04 15:19:23 +0900

    id값 초기화 메서드 추가

--- src/main/java/com/project/yucoordinator/domain/info/repository/CSEInfoRepository.java
+++ src/main/java/com/project/yucoordinator/domain/info/repository/CSEInfoRepository.java
@@ -2,9 +2,17 @@ package com.project.yucoordinator.domain.info.repository;
 
 import com.project.yucoordinator.domain.info.entity.CSEInfoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface CSEInfoRepository extends JpaRepository<CSEInfoEntity, Long> {
     Optional<CSEInfoEntity> findByTitle(String title);
+
+    @Modifying
+    @Transactional
+    @Query(value = "ALTER TABLE cse_info AUTO_INCREMENT = 1", nativeQuery = true)
+    void resetAutoIncrement();
 }
--- src/main/java/com/project/yucoordinator/domain/info/repository/YUInfoRepository.java
+++ src/main/java/com/project/yucoordinator/domain/info/repository/YUInfoRepository.java
@@ -2,9 +2,17 @@ package com.project.yucoordinator.domain.info.repository;
 
 import com.project.yucoordinator.domain.info.entity.YUInfoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface YUInfoRepository extends JpaRepository<YUInfoEntity, Long> {
     Optional<YUInfoEntity> findByTitle(String title);
+
+    @Modifying
+    @Transactional
+    @Query(value = "ALTER TABLE yu_info AUTO_INCREMENT = 1", nativeQuery = true)
+    void resetAutoIncrement();
 }
